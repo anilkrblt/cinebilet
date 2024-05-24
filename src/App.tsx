46421cd6aab59ff75df6edb799aecdf6a72a130b
Anıl karabulut <darkside_of_the_software@hotmail.com>
2024-05-24 13:47:08 +0300

    router eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
 // src/App.tsx
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Tabs from './components/Tabs';
-import MovieList from './components/MovieList';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import About from './components/About';
 
 const App: React.FC = () => {
   return (
-    <>
-      <div className="container-fluid text-center p-5">
-        <div className="row">
-          <Navbar />
-        </div>
-      </div>
-
-      <div className="container-fluid mt-5 text-center">
-        <div className="container mt-3">
-          <Tabs />
-          <div className="tab-content">
-            <div id="vizyondakiler" className="container tab-pane active">
-              <MovieList />
-            </div>
-            <div id="gelecek-filmler" className="container tab-pane fade">
-              <br />
-              <h3 className="text-white display-3">Gelecek Filmler</h3>
-              <p className="text-white display-4">Yakında...</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container text-center"></div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
-// src/components/NavBar.tsx
+// src/components/Navbar.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../logo.png';
 
 const Navbar: React.FC = () => {
@@ -7,32 +8,42 @@ const Navbar: React.FC = () => {
     <nav className="navbar navbar-expand-sm bg-danger fixed-top p-3">
       <div className="container">
         <div className="col">
-          <a className="navbar-brand hover-overlay hover-zoom hover-shadow ripple text-white" href="#">
-            <button type="button" className="btn btn-outlined-primary">Anasayfa</button>
+          <Link className="navbar-brand text-white" to="/home">
+            <button type="button" className="btn btn-outlined-primary">
+              Anasayfa
+            </button>
+          </Link>
+        </div>
+        <div className="col">
+          <a className="navbar-brand hover-overlay hover-zoom hover-shadow ripple  text-white" href="#">
+            <button type="button" className="btn btn-outlined-primary">
+              Sinemalarımız
+            </button>
           </a>
         </div>
         <div className="col">
-          <a className="navbar-brand hover-overlay hover-zoom hover-shadow ripple text-white" href="#">
-            <button type="button" className="btn btn-outlined-primary">Sinemalarımız</button>
+          <a className="navbar-brand " href="#">
+            <img src={logo} alt="Avatar Logo" style={{ width: "40px" }} className="rounded-pill" />
           </a>
         </div>
         <div className="col">
-          <a className="navbar-brand" href="#">
-            <img src={logo} alt="Avatar Logo" style={{ width: '40px' }} className="rounded-pill" />
-          </a>
+          <Link className="navbar-brand text-white" to="/about">
+            <button type="button" className="btn btn-outlined-primary">
+              Hakkımızda
+            </button>
+          </Link>
         </div>
         <div className="col">
-          <a className="navbar-brand text-white" href="#">
-            <button type="button" className="btn btn-outlined-primary">Hakkımızda</button>
+          <a className="navbar-brand  text-white " href="#">
+            <button type="button" className="btn btn-outlined-primary">
+              Kampanyalar
+            </button>
           </a>
         </div>
-        <div className="col">
-          <a className="navbar-brand text-white" href="#">
-            <button type="button" className="btn btn-outlined-primary">Kampanyalar</button>
-          </a>
-        </div>
-        <a className="navbar-brand text-white" href="#">
-          <button type="button" className="btn btn-outline-primary">Giriş Yap</button>
+        <a className="navbar-brand  text-white " href="#">
+          <button type="button" className="btn btn-outline-primary">
+            Giriş Yap
+          </button>
         </a>
       </div>
     </nav>
